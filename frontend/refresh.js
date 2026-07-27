55d87daa00e41f840913d95ed5110b38eea6a0ab
78w8yz8kfr-spec <78w8yz8kfr@privaterelay.appleid.com>
2026-07-27 21:13:38 +0200

    ESA Cache-Aktualisierung korrigieren

--- frontend/refresh.js
+++ frontend/refresh.js
@@ -7,7 +7,9 @@
     if ("caches" in window) {
       const keys = await caches.keys();
       await Promise.all(
-        keys.filter((key) => key.startsWith("schaefchen-")).map((key) => caches.delete(key))
+        keys
+          .filter((key) => key.startsWith("schaefchen-") || key.startsWith("esa-"))
+          .map((key) => caches.delete(key))
       );
     }
   } finally {
